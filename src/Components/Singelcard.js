cb8a2b4b4f771c3cab36278cd7969a3edb23b899
hamed <hamedtohiiidiii@gmail.com>
2024-06-16 20:55:27 +0330

    fix cover img

--- src/Components/Singelcard.js
+++ src/Components/Singelcard.js
@@ -1,19 +1,20 @@
 
 import './Singelcard.css'
 
-export default function Singelcard({card, handleChoice, flipped, disabled}) {
-  const handleClick = ()=>{
-    if(!disabled){
-      handleChoice(card) 
-    }
-    
+import coverImg from '../img/cover.png'
 
+export default function Singelcard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
   }
+
   return (
     <div className='card' >
-      <div className={flipped ? "flipped":""} >
+      <div className={flipped ? "flipped" : ""} >
         <img className='front' src={card.src} alt="card front" />
-        <img className='back' src="img/cover.png" onClick={handleClick} alt="card back" />
+        <img className='back' src={coverImg} onClick={handleClick} alt="card back" />
 
       </div>
     </div>
